ca072835ab2aed6bce7958abf78834042c744702
David Louie <dlouie1492@gmail.com>
2016-03-16 11:12:24 -0400

    Set Auto-Layout constraints for carrot and boombox results pages

---
+++ Carrot-App-3/InfoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  InfoViewController.swift
+//  Carrot-App-3
+//
+//  Created by Apprentice on 3/16/16.
+//  Copyright © 2016 Apprentice. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return false
+    }
+    
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.Portrait
+    }
+}
